eb6eed339b0c478e57f1a54c865741e9b72a53e2
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 16:24:38 +0200

    new commit

--- src/app/Components/Codedriving.tsx
+++ src/app/Components/Codedriving.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Codedriving() {
   return (
-    <div className=''>
+    <div className='myBgmainB'>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Heros from "./Components/Heros";
 import Winning from "./Components/Winning";
+import Codedriving from "./Components/Codedriving";
 
 export default function Home() {
   return (
    <>
    <Heros   />
    <Winning />
+   <Codedriving />
   </>
   );
 }

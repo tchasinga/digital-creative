61dd3601eeba20491962eca51e77cc6eef349160
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 13:47:14 +0200

    code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Heros from "./Components/Heros";
+import Winning from "./Components/Winning";
 
 export default function Home() {
   return (
    <>
    <Heros   />
+   <Winning />
   </>
   );
 }

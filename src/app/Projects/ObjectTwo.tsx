a622a2ba9645ae89e79ee2b900a9bc1545de1dd3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 18:17:52 +0200

    code is added

--- src/app/Projects/ObjectTwo.tsx
+++ src/app/Projects/ObjectTwo.tsx
@@ -1,6 +1,9 @@
 import { MdDesignServices } from "react-icons/md";
 import { SiAndroidstudio } from "react-icons/si";
+import { FaDigitalOcean } from "react-icons/fa";
 import { MdManageAccounts } from "react-icons/md";
+import { SiCodecov } from "react-icons/si";
+import { CgWebsite } from "react-icons/cg";
 
 const solution = [
   {
@@ -22,5 +25,23 @@ const solution = [
     Icon: <MdManageAccounts />,
     details:
       "Search Engine Optimization (SEO) is a crucial aspect of digital marketing aimed at improving a website's visibility on search engines like Google. A well-executed SEO strategy helps drive organic (non-paid) traffic to a website, increases its online presence, and enhances its overall performance. ",
+  },
+  {
+    name: "Digital Marketing",
+    Icon: <FaDigitalOcean />,
+    details:
+      "Digital marketing is a broad term that encompasses all marketing efforts conducted through digital channels, such as the internet, social media, email, search engines, and various other digital platforms. The goal of digital marketing is to reach and engage with a target audience online, drive brand awareness, generate leads, and ultimately convert leads into customer",
+  },
+  {
+    name: "Graphics design",
+    Icon: <SiCodecov />,
+    details:
+      "Graphic design is a creative process that involves combining visual elements, typography, images, and other design elements to communicate a message or convey information visually. Graphic designers use various tools and techniques to create visually appealing and effective designs for a wide range of purposes",
+  },
+  {
+    name: "Website",
+    Icon: <CgWebsite />,
+    details:
+      "Creating a website involves several steps, from planning and design to development and deployment",
   },
 ];

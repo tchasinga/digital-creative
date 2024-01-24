85339ea94ff58705d59d826e61fabf1c175b7cc8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 14:29:36 +0200

    new code is added now

--- src/app/Components/Winning.tsx
+++ src/app/Components/Winning.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from "next/image";
+import myBusness from '../Images/pexelskindelmedia.jpg';
 
 export default function Winning() {
   return (
@@ -8,6 +10,9 @@ export default function Winning() {
           <div className="relative">
             <div className='w-10 h-[655px] bg-slate-950 rounded-full sticker rotate-[35deg]'></div>
             <div className="w-[500px] h-[500px] rounded-[50%] bg-blue-400 absolute bottom-[10%] right-[70%]">
+                <div className="top-[1%] left-[5%] absolute">
+                <Image src={myBusness} alt='' className='object-cover rounded-[50%] w-[500px] h-[500px] '/>
+                </div>
             </div>
           </div>
            

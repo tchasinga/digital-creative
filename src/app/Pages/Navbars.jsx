895a7deb86478ada2fe05e1dae01b6606db908bb
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 10:55:12 +0200

    Basics code

--- src/app/Pages/Navbars.jsx
+++ src/app/Pages/Navbars.jsx
@@ -7,7 +7,7 @@ export default function Navbars() {
   return (
     <div className='max-w-5xl mx-auto p-4 flex items-center justify-between w-full'>
 
-     <div className="flex items-center">
+     <div className="flex items-center gap-2">
         <TbBrandDolbyDigital className="text-4xl text-black" />
         <p>Digital</p>
      </div>

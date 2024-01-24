d2f7b413a949bcc3d3cd92978250c41f56115bc8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 11:07:46 +0200

    new code is added

--- src/app/Components/Heros.jsx
+++ src/app/Components/Heros.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 export default function Heros() {
   return (
-    <div className='w-full '>
+    <div className='w-full relative'>
+
+        <div className="">
+             {/* First part of is added now...*/}
+             
+        </div>
+
     </div>
   )
 }

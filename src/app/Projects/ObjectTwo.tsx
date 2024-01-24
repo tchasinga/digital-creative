22a675a3c633590a6ebad4135047b0d5cf9fe53d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 18:06:40 +0200

    new code

---
+++ src/app/Projects/ObjectTwo.tsx
@@ -0,0 +1,26 @@
+import { MdDesignServices } from "react-icons/md";
+import { SiAndroidstudio } from "react-icons/si";
+import { MdManageAccounts } from "react-icons/md";
+
+const solution = [
+  {
+    name: "UI/UX Design",
+    Icon: <MdDesignServices />,
+    details:
+      "By combining thoughtful user research, strategic design decisions, and continuous testing and refinement, UI/UX designers can create products that not only look good but also provide a positive and efficient user experience.",
+  },
+
+  {
+    name: "App development",
+    Icon: <SiAndroidstudio />,
+    details:
+      "The specific details of each step can vary depending on the platform, technology stack, and development methodology chosen. Whether you're developing a mobile app, a web app, or a desktop application, a well-structured development process is key to creating a successful and sustainable product.",
+  },
+
+  {
+    name: "SEO Markting",
+    Icon: <MdManageAccounts />,
+    details:
+      "Search Engine Optimization (SEO) is a crucial aspect of digital marketing aimed at improving a website's visibility on search engines like Google. A well-executed SEO strategy helps drive organic (non-paid) traffic to a website, increases its online presence, and enhances its overall performance. ",
+  },
+];

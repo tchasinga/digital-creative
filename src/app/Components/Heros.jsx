39b4dcba05e7a3affd24930bee3376b075550275
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 11:17:45 +0200

    new code is anow added

--- src/app/Components/Heros.jsx
+++ src/app/Components/Heros.jsx
@@ -9,7 +9,15 @@ export default function Heros() {
             <div>
               {/* First part of is added now...*/}
               <div className="">
-                
+                <h3>#1 SEO COMPANY IN TANZANIA</h3>
+              </div>
+
+              <div className="">
+                <h1>We Grow Your Revenue <b/>With Results-driven SEO</h1>
+              </div>
+
+              <div className="">
+                <p>Our SEO services help drive traffic to your website by improving keyword rankings in search engines..</p>
               </div>
             </div>
 

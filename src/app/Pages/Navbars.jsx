6c45043120250df75ba8cf339ad73ade1838ad26
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 10:31:21 +0200

    code is added

--- src/app/Pages/Navbars.jsx
+++ src/app/Pages/Navbars.jsx
@@ -3,10 +3,10 @@ import { TbBrandDolbyDigital } from "react-icons/tb";
 
 export default function Navbars() {
   return (
-    <div className='max-w-4xl mx-auto'>
+    <div className='max-w-5xl mx-auto p-4'>
 
      <div className="">
-        <TbBrandDolbyDigital className="text-4xl text-white" />
+        <TbBrandDolbyDigital className="text-4xl text-black" />
      </div>
 
     </div>

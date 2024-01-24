ccbbed273de969166665d8da3b780eb8606e125a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 10:52:29 +0200

    code is added

--- src/app/Pages/Navbars.jsx
+++ src/app/Pages/Navbars.jsx
@@ -5,7 +5,7 @@ import { MdMarkEmailUnread } from "react-icons/md";
 
 export default function Navbars() {
   return (
-    <div className='max-w-5xl mx-auto p-4 flex items-center justify-between'>
+    <div className='max-w-5xl mx-auto p-4 flex items-center justify-between w-full'>
 
      <div className="">
         <TbBrandDolbyDigital className="text-4xl text-black" />

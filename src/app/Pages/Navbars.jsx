70bc28f82e059244ebd635d0eb0b07efe9761a10
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 10:42:30 +0200

    new code is added now

--- src/app/Pages/Navbars.jsx
+++ src/app/Pages/Navbars.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 import { TbBrandDolbyDigital } from "react-icons/tb";
+import { FaPhone} from "react-icons/fa6";
+import { MdMarkEmailUnread } from "react-icons/md";
 
 export default function Navbars() {
   return (
-    <div className='max-w-5xl mx-auto p-4'>
+    <div className='max-w-5xl mx-auto p-4 flex items-center justify-between'>
 
      <div className="">
         <TbBrandDolbyDigital className="text-4xl text-black" />
      </div>
 
+     <div className="">
+        <ul className='flex items-center gap-4'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+            <li>Page</li>
+            <li>Conctact</li>
+        </ul>
+     </div>
+
+     <div className="">
+        <ul className='flex items-center justify-center gap-4'>
+            <li className='flex items-center gap-2'>
+                <p><FaPhone className="text-lg text-black" /> </p>
+                <p>+250 7872 890 28</p>
+            </li>
+            <li>
+                <p><MdMarkEmailUnread className="text-lg text-black" /> </p>
+            </li>
+            <li>Free Analysis</li>
+        </ul>
+     </div>
     </div>
   )
 }

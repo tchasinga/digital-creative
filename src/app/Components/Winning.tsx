07d8537f49a5f24d527c3d6312c617f9562e0868
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 14:03:31 +0200

    new

--- src/app/Components/Winning.tsx
+++ src/app/Components/Winning.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Winning() {
   return (
-    <div className='w-full bg-white'>
-       
+    <div className='w-full bg-white h-screen'>
+       <div className="">
+           {/* first code is added...*/}
+          <div className="relative">
+            <div className='w-10 h-[555px] bg-slate-950 rounded-full sticker'></div>  
+          </div>
+           
+            {/* Seconds div is show-up */}
+       </div>
     </div>
   )
 }

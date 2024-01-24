45454d358d6a89ef6412a10735523ed456dd4ab7
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 16:32:23 +0200

    new code is added

--- src/app/Components/Codedriving.tsx
+++ src/app/Components/Codedriving.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 export default function Codedriving() {
   return (
     <div className='myBgmainB'>
+      <div className="max-w-5xl mx-auto">
+      <div className="flex items-center text-slate-400 gap-3">
+        <hr className='w-[100px]'/>
+        <p>Statics</p>
+        </div>
+
+        
+      </div>
     </div>
   )
 }

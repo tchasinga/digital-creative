5bcefb365f1ba88d3a98eec4b533a752f33a44ea
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-24 14:20:29 +0200

    new code is now added

--- src/app/Components/Winning.tsx
+++ src/app/Components/Winning.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 export default function Winning() {
   return (
     <div className='w-full bg-white h-screen'>
-       <div className="">
+       <div className="max-w-5xl mx-auto">
            {/* first code is added...*/}
           <div className="relative">
-            <div className='w-10 h-[555px] bg-slate-950 rounded-full sticker'></div>  
+            <div className='w-10 h-[655px] bg-slate-950 rounded-full sticker rotate-[35deg]'></div>
+            <div className="w-[500px] h-[500px] rounded-[50%] bg-blue-400 absolute bottom-[10%] right-[70%]">
+            </div>
           </div>
            
             {/* Seconds div is show-up */}

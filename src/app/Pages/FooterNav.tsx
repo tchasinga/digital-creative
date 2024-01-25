8a423fd8d69844320d3256b52d7b4edbe728dc01
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-25 15:27:10 +0200

    new code is added now

--- src/app/Pages/FooterNav.tsx
+++ src/app/Pages/FooterNav.tsx
@@ -9,8 +9,24 @@ import { RiContactsFill } from "react-icons/ri";
 
 export default function FooterNav() {
   return (
-    <div>
-      
-    </div>
+    <div className="footContainer  bg-blue-300 fixed bottom-0 left-0 right-0">
+            <div className=" flex py-5 justify-evenly items-center ">
+                 <div className="text-2xl">
+                    <IoIosHome/>
+                 </div>
+                 <div className="text-2xl">
+                    <IoMdInformationCircleOutline/>
+                 </div>
+                 <div className="text-2xl">
+                    <RiCustomerServiceFill/>
+                 </div>
+                 <div className="text-2xl">
+                    <SiCloudflarepages/>
+                 </div>
+                 <div className="text-2xl">
+                    <RiContactsFill/>
+                 </div>
+            </div>
+        </div>
   )
 }

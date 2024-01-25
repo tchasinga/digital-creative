cb9c304624ec1506d32dd93db4bcd2f79633638a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-25 09:02:48 +0200

    new code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Heros from "./Components/Heros";
 import Winning from "./Components/Winning";
 import Codedriving from "./Components/Codedriving";
 import Ourservices from "./Components/Ourservices";
+import Swipper from "./Components/Swipper";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
    <Winning />
    <Codedriving />
    <Ourservices />
+   <Swipper />
   </>
   );
 }

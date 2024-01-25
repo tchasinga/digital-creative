e3d513131e78c910b538e8127f083849e577d6c4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-25 15:02:13 +0200

    new code

--- src/app/Pages/FooterNav.tsx
+++ src/app/Pages/FooterNav.tsx
@@ -1,4 +1,11 @@
+"use client"
+
 import React from 'react'
+import { IoIosHome } from "react-icons/io";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { RiCustomerServiceFill } from "react-icons/ri";
+import { SiCloudflarepages } from "react-icons/si";
+import { RiContactsFill } from "react-icons/ri";
 
 export default function FooterNav() {
   return (

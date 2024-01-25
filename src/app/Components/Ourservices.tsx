dd76880fc08e1fdb22d9ed59dae3943575ba10c9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-25 08:15:55 +0200

    new changes

--- src/app/Components/Ourservices.tsx
+++ src/app/Components/Ourservices.tsx
@@ -4,13 +4,17 @@ export default function Ourservices() {
   return (
     <div className='bgCodeOfBackground'>
      <div className="max-w-5xl mx-auto">
-        <div className="flex items-center text-slate-400 gap-3 py-3">
+       <div className="flex flex-col justify-center text-center">
+       <div className="flex items-center text-slate-400 gap-3 py-3">
          <hr className='w-[100px]'/>
          <p className='text-orange-800'>Services</p>
         </div>
         <div className="text-slate-900 text-3xl">
              <h2>What solution we provide to <br /> Our valued customers</h2>
         </div>
+       </div>
+
+       
     </div>
     </div>
   )
